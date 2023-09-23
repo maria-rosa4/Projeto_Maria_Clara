9a183e93bb442cc3869c7c777b158552c6d5cf35
Maria Clara <maria.rosa4@estudante.ufla.br>
2023-09-23 18:51:04 -0300

    Criacao da Classe C

---
+++ classC.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+class C {
+private:
+    int C1;
+    float C2;
+
+
+public:
+    // Construtor
+    C(int c1, float c2) : C1(c1), C2(c2) {}
+
+
+    // Getters e setters
+    int getC1() const {
+        return C1;
+    }
+
+
+    void setC1(int C1) {
+        C1 = c1;
+    }
+
+
+    float getC2() const {
+        return C2;
+    }
+
+
+    void setC2(float c2) {
+        C2 = c2;
+    }
+
+
+    // Métodos da classe C
+    void MC1() {
+        cout << "Método MC1() da classe C" << endl;
+    }
+
+
+    void MC2() {
+        cout << "Método MC2() da classe C" << endl;
+    }
+};
+
+
+int main() {
+    C objetoC(30, 2.500);
+
+
+    objetoC.MC1();
+    objetoC.MC2();
+
+
+    return 0;
+}
+
+
